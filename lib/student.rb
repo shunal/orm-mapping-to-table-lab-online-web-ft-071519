6e498f94abf5a9ec6044b077cfeb1a4d24c9ab0b
Steven Hunal <stevehunal@gmail.com>
2019-09-10 00:59:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,11 @@
 class Student
 
+attr_accessor :name, :grade, :id 
+
+def initialize(name, grade)
+end
+
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
