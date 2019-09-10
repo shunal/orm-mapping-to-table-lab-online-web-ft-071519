5b5e6f555339bc7f3ebfec1b9e90da7e67a9f134
Steven Hunal <stevehunal@gmail.com>
2019-09-10 01:01:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,6 +7,17 @@ def initialize(name, grade)
   @grade = grade
 end
 
+def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
+
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
