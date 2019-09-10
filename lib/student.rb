f978655bf6c867915756d69a50fef035096fd7ea
Steven Hunal <stevehunal@gmail.com>
2019-09-10 01:13:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,7 @@
 class Student
 
-attr_accessor :name, :grade
-attr_reader :id
-def initialize(name, grade)
+attr_accessor :name, :grade, :id
+def initialize(name, grade, id=nil)
   @name = name 
   @grade = grade
   @id = id
